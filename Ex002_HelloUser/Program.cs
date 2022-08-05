8254bc7a7fa02dd2f4dc6d8e59b28c6530199091
OlegSam2 <doorsmania@mail.ru>
2022-08-05 22:26:02 +0300

    Example Hello user q q Q

---
+++ Ex002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите ваше имя "); // 	
+string username = Console.ReadLine(); // возьми то что справа и положи в то что слево (присвоение)	
+Console.Write("Привет, "); // выводим приветствие на терминал	
+Console.Write(username); // присвоение переменной username было в первой строчке	
+
